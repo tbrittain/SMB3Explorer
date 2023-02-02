24eb439b2bd3075621d32d86dcb079cbcac95e97
tbrittain <treybrittain@gmail.com>
2023-02-01 18:39:55 -0600

    Add SetupDbConnection and handle errors

--- SMB3Explorer/MainWindow.xaml.cs
+++ SMB3Explorer/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using Ionic.Zlib;
 using Microsoft.Data.Sqlite;
 using Microsoft.Win32;
 
@@ -9,25 +13,27 @@ namespace SMB3Explorer;
 
 public partial class MainWindow
 {
+    private const string DefaultSaveFileName = "savedata.sav";
+
     public MainWindow()
     {
         InitializeComponent();
-        
+
         if (Environment.OSVersion.Platform != PlatformID.Win32NT)
         {
             MessageBox.Show("This application is only supported on Windows.");
             Environment.Exit(1);
         }
     }
-    
-    private SqliteConnection? Connection { get; set; } = null;
+
+    private SqliteConnection? Connection { get; set; }
 
     private static string GetUserProvidedFile(string directoryPath)
     {
         var openFileDialog = new OpenFileDialog
         {
             Filter = "Save files (*.sav)",
-            InitialDirectory = directoryPath,
+            InitialDirectory = directoryPath
         };
 
         return openFileDialog.ShowDialog() != true
@@ -35,9 +41,7 @@ public partial class MainWindow
             : openFileDialog.FileName;
     }
 
-    private const string DefaultSaveFileName = "savedata.sav";
-
-    private static string GetSaveFile()
+    private static Task<string> GetSaveFile()
     {
         var baseDirectoryPath = Path.Combine(Environment.GetFolderPath(
                 Environment.SpecialFolder.LocalApplicationData),
@@ -49,9 +53,9 @@ public partial class MainWindow
                 MessageBox.Show("Default save file directory does not exist. " +
                                 "Would you like to select a save file directly?",
                     "Default location not found", MessageBoxButton.YesNo);
-            return result == MessageBoxResult.Yes
+            return Task.FromResult(result == MessageBoxResult.Yes
                 ? GetUserProvidedFile(baseDirectoryPath)
-                : string.Empty;
+                : string.Empty);
         }
 
         var subdirectories = Directory.GetDirectories(baseDirectoryPath);
@@ -67,27 +71,90 @@ public partial class MainWindow
         {
             var defaultFilePath = Path.Combine(steamUserDirectory, DefaultSaveFileName);
 
-            if (File.Exists(defaultFilePath)) return defaultFilePath;
+            if (File.Exists(defaultFilePath)) return Task.FromResult(defaultFilePath);
         }
 
         var result2 = MessageBox.Show("Default file does not exist. " +
                                       "Would you like to select a save file directly?",
             "Default file not found", MessageBoxButton.YesNo);
 
-        return result2 == MessageBoxResult.Yes
+        return Task.FromResult(result2 == MessageBoxResult.Yes
             ? GetUserProvidedFile(steamUserDirectory ?? baseDirectoryPath)
-            : string.Empty;
+            : string.Empty);
     }
 
-    private void Button_OnClick(object sender, RoutedEventArgs e)
+    private async void Button_OnClick(object sender, RoutedEventArgs e)
     {
-        var filePath = GetSaveFile();
-        if (string.IsNullOrEmpty(filePath)) return;
-        
-        using (var compressedStream = File.OpenRead(filePath))
-        using (var decompressedStream = new MemoryStream())
+        Mouse.OverrideCursor = Cursors.Wait;
+
+        var filePath = await GetSaveFile();
+        if (string.IsNullOrEmpty(filePath))
         {
-            //TODO: Decompress stream and write to decompressedStream
+            Mouse.OverrideCursor = Cursors.Arrow;
+            return;
         }
+
+        var (ok, exception) = await SetupDbConnection(filePath);
+        Mouse.OverrideCursor = Cursors.Arrow;
+
+        if (!ok)
+        {
+            const string initialMessage = "Failed to connect to SMB3 database. " +
+                                          "A full stack trace has been copied to your clipboard. " +
+                                          "Press OK to report this issue on GitHub.";
+
+            var formattedMessage = $"{initialMessage}{Environment.NewLine}{exception?.Message ?? "Unknown error"}";
+
+            var openBrowser = MessageBox.Show(this, formattedMessage,
+                "Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+
+            Clipboard.SetText(exception?.StackTrace ?? "Unknown error");
+
+            if (openBrowser != MessageBoxResult.OK) return;
+
+            const string url = "https://github.com/tbrittain/SMB3Explorer2/issues/new";
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
+
+            return;
+        }
+
+        MessageBox.Show("Successfully connected to SMB3 database");
+    }
+
+    private Task<(bool, Exception?)> SetupDbConnection(string filePath)
+    {
+        try
+        {
+            using var compressedStream = File.OpenRead(filePath);
+            using var zlibStream = new ZlibStream(compressedStream, CompressionMode.Decompress);
+            using var decompressedStream = new MemoryStream();
+
+            zlibStream.CopyTo(decompressedStream);
+            decompressedStream.Position = 0;
+
+            var decompressedFileName = $"smb3_explorer_{DateTime.Now:yyyyMMddHHmmssfff}.sqlite";
+            var decompressedFilePath = Path.Combine(Path.GetTempPath(), decompressedFileName);
+
+            using (var fileStream = File.Create(decompressedFilePath))
+            {
+                decompressedStream.CopyTo(fileStream);
+            }
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder
+            {
+                DataSource = decompressedFilePath,
+                Mode = SqliteOpenMode.ReadOnly,
+                Cache = SqliteCacheMode.Shared
+            };
+
+            Connection = new SqliteConnection(connectionStringBuilder.ToString());
+            Connection.Open();
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult((false, (Exception?) e));
+        }
+
+        return Task.FromResult((true, null as Exception));
     }
 }
