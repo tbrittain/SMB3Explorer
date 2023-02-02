b3f2a9039caf1e5f252208b120301fa898e0037d
tbrittain <treybrittain@gmail.com>
2023-02-01 21:29:01 -0600

    Extract save file logic to SaveFile utility class

---
+++ SMB3Explorer/Utils/SaveFile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace SMB3Explorer.Utils;
+
+public static class SaveFile
+{
+    private const string DefaultSaveFileName = "savedata.sav";
+    
+    public static Task<string> GetSaveFilePath()
+    {
+        var baseDirectoryPath = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData),
+            "Metalhead", "Super Mega Baseball 3");
+
+        if (!Directory.Exists(baseDirectoryPath))
+        {
+            var result =
+                MessageBox.Show("Default save file directory does not exist. " +
+                                "Would you like to select a save file directly?",
+                    "Default location not found", MessageBoxButton.YesNo);
+            return Task.FromResult(result == MessageBoxResult.Yes
+                ? GetUserProvidedFile(baseDirectoryPath)
+                : string.Empty);
+        }
+
+        var subdirectories = Directory.GetDirectories(baseDirectoryPath);
+        // TODO: Handle case where there are no subdirectories
+        var steamUserDirectory = subdirectories.SingleOrDefault(x =>
+        {
+            var lastBackslashIndex = x.LastIndexOf('\\');
+            var lastPart = x[(lastBackslashIndex + 1)..];
+            return long.TryParse(lastPart, out _);
+        });
+
+        if (steamUserDirectory != null)
+        {
+            var defaultFilePath = Path.Combine(steamUserDirectory, DefaultSaveFileName);
+
+            if (File.Exists(defaultFilePath)) return Task.FromResult(defaultFilePath);
+        }
+
+        var result2 = MessageBox.Show("Default file does not exist. " +
+                                      "Would you like to select a save file directly?",
+            "Default file not found", MessageBoxButton.YesNo);
+
+        return Task.FromResult(result2 == MessageBoxResult.Yes
+            ? GetUserProvidedFile(steamUserDirectory ?? baseDirectoryPath)
+            : string.Empty);
+    }
+    
+    private static string GetUserProvidedFile(string directoryPath)
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            Filter = "Save files (*.sav)",
+            InitialDirectory = directoryPath
+        };
+
+        return openFileDialog.ShowDialog() != true
+            ? string.Empty
+            : openFileDialog.FileName;
+    }
+}
--- SMB3Explorer/ViewModels/MainWindowViewModel.cs
+++ SMB3Explorer/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.Win32;
 using SMB3Explorer.Services;
+using SMB3Explorer.Utils;
 
 namespace SMB3Explorer.ViewModels;
 
@@ -22,69 +20,12 @@ public class MainWindowViewModel
     public ICommand SelectSaveFileCommand { get; }
     
     private IDataService DataService { get; }
-    
-    private const string DefaultSaveFileName = "savedata.sav";
 
-    private static string GetUserProvidedFile(string directoryPath)
-    {
-        var openFileDialog = new OpenFileDialog
-        {
-            Filter = "Save files (*.sav)",
-            InitialDirectory = directoryPath
-        };
-
-        return openFileDialog.ShowDialog() != true
-            ? string.Empty
-            : openFileDialog.FileName;
-    }
-
-    private static Task<string> GetSaveFile()
-    {
-        var baseDirectoryPath = Path.Combine(Environment.GetFolderPath(
-                Environment.SpecialFolder.LocalApplicationData),
-            "Metalhead", "Super Mega Baseball 3");
-
-        if (!Directory.Exists(baseDirectoryPath))
-        {
-            var result =
-                MessageBox.Show("Default save file directory does not exist. " +
-                                "Would you like to select a save file directly?",
-                    "Default location not found", MessageBoxButton.YesNo);
-            return Task.FromResult(result == MessageBoxResult.Yes
-                ? GetUserProvidedFile(baseDirectoryPath)
-                : string.Empty);
-        }
-
-        var subdirectories = Directory.GetDirectories(baseDirectoryPath);
-        // TODO: Handle case where there are no subdirectories
-        var steamUserDirectory = subdirectories.SingleOrDefault(x =>
-        {
-            var lastBackslashIndex = x.LastIndexOf('\\');
-            var lastPart = x[(lastBackslashIndex + 1)..];
-            return long.TryParse(lastPart, out _);
-        });
-
-        if (steamUserDirectory != null)
-        {
-            var defaultFilePath = Path.Combine(steamUserDirectory, DefaultSaveFileName);
-
-            if (File.Exists(defaultFilePath)) return Task.FromResult(defaultFilePath);
-        }
-
-        var result2 = MessageBox.Show("Default file does not exist. " +
-                                      "Would you like to select a save file directly?",
-            "Default file not found", MessageBoxButton.YesNo);
-
-        return Task.FromResult(result2 == MessageBoxResult.Yes
-            ? GetUserProvidedFile(steamUserDirectory ?? baseDirectoryPath)
-            : string.Empty);
-    }
-    
     private async Task SetSaveFile()
     {
         Mouse.OverrideCursor = Cursors.Wait;
 
-        var filePath = await GetSaveFile();
+        var filePath = await SaveFile.GetSaveFilePath();
         if (string.IsNullOrEmpty(filePath))
         {
             Mouse.OverrideCursor = Cursors.Arrow;
