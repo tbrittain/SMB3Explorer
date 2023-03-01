ca3778d36c72c2019df413e5b221be74fc719285
tbrittain <treybrittain@gmail.com>
2023-02-28 23:27:03 -0600

    Add BattingSeasonStatisticMapper

---
+++ SMB3Explorer/Models/Mappings/BattingSeasonStatisticMapper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using AutoMapper;
+using Microsoft.Data.Sqlite;
+using SMB3Explorer.Utils;
+
+namespace SMB3Explorer.Models.Mappings;
+
+public class BattingSeasonStatisticMapper : Profile
+{
+    public BattingSeasonStatisticMapper()
+    {
+        CreateMap<SqliteDataReader, BattingSeasonStatistic>()
+            .ForMember(dest => dest.PlayerId,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["baseballPlayerGUID"].ToString())
+                        ? (Guid?) null
+                        : Encoding.UTF8.GetBytes(src["baseballPlayerGUID"].ToString()!).ToGuid()))
+            .ForMember(dest => dest.FirstName,
+                opt => opt.MapFrom(src => src["firstName"].ToString()))
+            .ForMember(dest => dest.LastName,
+                opt => opt.MapFrom(src => src["lastName"].ToString()))
+            .ForMember(dest => dest.PositionNumber,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["primaryPosition"].ToString())
+                        ? 0
+                        : int.Parse(src["primaryPosition"].ToString()!)))
+            .ForMember(dest => dest.SecondaryPositionNumber,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["secondaryPosition"].ToString())
+                        ? (int?) null
+                        : int.Parse(src["secondaryPosition"].ToString()!)))
+            .ForMember(dest => dest.CurrentTeam,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["currentTeam"].ToString())
+                        ? null
+                        : src["currentTeam"].ToString()))
+            .ForMember(dest => dest.PreviousTeam,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["previousTeam"].ToString())
+                        ? null
+                        : src["previousTeam"].ToString()))
+            .ForMember(dest => dest.GamesPlayed,
+                opt => opt.MapFrom(src => int.Parse(src["gamesPlayed"].ToString()!)))
+            .ForMember(dest => dest.GamesBatting,
+                opt => opt.MapFrom(src => int.Parse(src["gamesBatting"].ToString()!)))
+            .ForMember(dest => dest.AtBats,
+                opt => opt.MapFrom(src => int.Parse(src["atBats"].ToString()!)))
+            .ForMember(dest => dest.Runs,
+                opt => opt.MapFrom(src => int.Parse(src["runs"].ToString()!)))
+            .ForMember(dest => dest.Hits,
+                opt => opt.MapFrom(src => int.Parse(src["hits"].ToString()!)))
+            .ForMember(dest => dest.Doubles,
+                opt => opt.MapFrom(src => int.Parse(src["doubles"].ToString()!)))
+            .ForMember(dest => dest.Triples,
+                opt => opt.MapFrom(src => int.Parse(src["triples"].ToString()!)))
+            .ForMember(dest => dest.HomeRuns,
+                opt => opt.MapFrom(src => int.Parse(src["homeruns"].ToString()!)))
+            .ForMember(dest => dest.RunsBattedIn,
+                opt => opt.MapFrom(src => int.Parse(src["rbi"].ToString()!)))
+            .ForMember(dest => dest.StolenBases,
+                opt => opt.MapFrom(src => int.Parse(src["stolenBases"].ToString()!)))
+            .ForMember(dest => dest.CaughtStealing,
+                opt => opt.MapFrom(src => int.Parse(src["caughtStealing"].ToString()!)))
+            .ForMember(dest => dest.Walks,
+                opt => opt.MapFrom(src => int.Parse(src["baseOnBalls"].ToString()!)))
+            .ForMember(dest => dest.Strikeouts,
+                opt => opt.MapFrom(src => int.Parse(src["strikeOuts"].ToString()!)))
+            .ForMember(dest => dest.HitByPitch,
+                opt => opt.MapFrom(src => int.Parse(src["hitByPitch"].ToString()!)))
+            .ForMember(dest => dest.SacrificeHits,
+                opt => opt.MapFrom(src => int.Parse(src["sacrificeHits"].ToString()!)))
+            .ForMember(dest => dest.SacrificeFlies,
+                opt => opt.MapFrom(src => int.Parse(src["sacrificeFlies"].ToString()!)))
+            .ForMember(dest => dest.Errors,
+                opt => opt.MapFrom(src => int.Parse(src["errors"].ToString()!)))
+            .ForMember(dest => dest.PassedBalls,
+                opt => opt.MapFrom(src => int.Parse(src["passedBalls"].ToString()!)))
+            .ForMember(dest => dest.CompletionDate,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["completionDate"].ToString())
+                        ? (DateTime?) null
+                        : DateTime.Parse(src["completionDate"].ToString()!)))
+            .ForMember(dest => dest.SeasonId,
+                opt => opt.MapFrom(src =>
+                    string.IsNullOrEmpty(src["seasonId"].ToString())
+                        ? (int?) null
+                        : int.Parse(src["seasonId"].ToString()!)));
+    }
+}
--- SMB3Explorer/Services/DataService/DataServiceFranchiseSeasons.cs
+++ SMB3Explorer/Services/DataService/DataServiceFranchiseSeasons.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.Data.Sqlite;
 using SMB3Explorer.Models;
+using SMB3Explorer.Models.Mappings;
 using SMB3Explorer.Utils;
 
 namespace SMB3Explorer.Services.DataService;
@@ -27,57 +29,19 @@ public partial class DataService
         {
             Value = _applicationContext.SelectedFranchise!.FranchiseId.ToBlob()
         });
+        
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<BattingSeasonStatisticMapper>();
+        });
+
+        var mapper = new Mapper(config);
 
         var reader = await command.ExecuteReaderAsync();
 
         while (reader.Read())
         {
-            var positionPlayerStatistic = new BattingSeasonStatistic();
-
-            positionPlayerStatistic.FirstName = reader["firstName"].ToString()!;
-            positionPlayerStatistic.LastName = reader["lastName"].ToString()!;
-
-            var position = reader["primaryPosition"].ToString();
-            positionPlayerStatistic.PositionNumber = position is null ? 0 : int.Parse(position);
-
-            positionPlayerStatistic.SecondaryPositionNumber =
-                string.IsNullOrEmpty(reader["secondaryPosition"].ToString()!)
-                    ? null
-                    : int.Parse(reader["secondaryPosition"].ToString()!);
-            positionPlayerStatistic.CurrentTeam = string.IsNullOrEmpty(reader["currentTeam"].ToString()!)
-                ? null
-                : reader["currentTeam"].ToString()!;
-            positionPlayerStatistic.GamesPlayed = int.Parse(reader["gamesPlayed"].ToString()!);
-            positionPlayerStatistic.GamesBatting = int.Parse(reader["gamesBatting"].ToString()!);
-            positionPlayerStatistic.AtBats = int.Parse(reader["atBats"].ToString()!);
-            positionPlayerStatistic.Runs = int.Parse(reader["runs"].ToString()!);
-            positionPlayerStatistic.Hits = int.Parse(reader["hits"].ToString()!);
-            positionPlayerStatistic.Doubles = int.Parse(reader["doubles"].ToString()!);
-            positionPlayerStatistic.Triples = int.Parse(reader["triples"].ToString()!);
-            positionPlayerStatistic.HomeRuns = int.Parse(reader["homeruns"].ToString()!);
-            positionPlayerStatistic.RunsBattedIn = int.Parse(reader["rbi"].ToString()!);
-            positionPlayerStatistic.StolenBases = int.Parse(reader["stolenBases"].ToString()!);
-            positionPlayerStatistic.CaughtStealing = int.Parse(reader["caughtStealing"].ToString()!);
-            positionPlayerStatistic.Walks = int.Parse(reader["baseOnBalls"].ToString()!);
-            positionPlayerStatistic.Strikeouts = int.Parse(reader["strikeOuts"].ToString()!);
-            positionPlayerStatistic.HitByPitch = int.Parse(reader["hitByPitch"].ToString()!);
-            positionPlayerStatistic.SacrificeHits = int.Parse(reader["sacrificeHits"].ToString()!);
-            positionPlayerStatistic.SacrificeFlies = int.Parse(reader["sacrificeFlies"].ToString()!);
-            positionPlayerStatistic.Errors = int.Parse(reader["errors"].ToString()!);
-            positionPlayerStatistic.PassedBalls = int.Parse(reader["passedBalls"].ToString()!);
-            positionPlayerStatistic.CompletionDate = string.IsNullOrEmpty(reader["completionDate"].ToString()!)
-                ? null
-                : DateTime.Parse(reader["completionDate"].ToString()!);
-            positionPlayerStatistic.SeasonId = int.Parse(reader["seasonId"].ToString()!);
-            positionPlayerStatistic.PlayerId = reader["baseballPlayerGUID"] is not byte[] bytes ? null : bytes.ToGuid();
-
-            if (isRegularSeason)
-            {
-                positionPlayerStatistic.PreviousTeam = string.IsNullOrEmpty(reader["previousTeam"].ToString()!)
-                    ? null
-                    : reader["previousTeam"].ToString()!;
-            }
-
+            var positionPlayerStatistic = mapper.Map<BattingSeasonStatistic>(reader);
             yield return positionPlayerStatistic;
         }
     }
