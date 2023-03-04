91312f3a79dad7206b5dbfd9e39ab5e308050d2f
tbrittain <treybrittain@gmail.com>
2023-03-04 00:04:53 -0600

    Remove AutoMapper mappings until later date

--- SMB3Explorer/Services/DataService/DataServiceFranchiseCareer.cs
+++ SMB3Explorer/Services/DataService/DataServiceFranchiseCareer.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using AutoMapper;
 using Microsoft.Data.Sqlite;
 using SMB3Explorer.Models;
-using SMB3Explorer.Models.Mappings;
 using SMB3Explorer.Utils;
 
 namespace SMB3Explorer.Services.DataService;
@@ -23,19 +21,58 @@ public partial class DataService
         {
             Value = _applicationContext.SelectedFranchise!.LeagueId.ToBlob()
         });
-        
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile<CareerBattingStatisticMapper>();
-        });
-        
-        var mapper = new Mapper(config);
 
         var reader = await command.ExecuteReaderAsync();
 
         while (reader.Read())
         {
-            var positionPlayer = mapper.Map<CareerBattingStatistic>(reader);
+            var positionPlayer = new CareerBattingStatistic();
+
+            positionPlayer.AggregatorId = int.Parse(reader["aggregatorID"].ToString()!);
+            positionPlayer.StatsPlayerId = int.Parse(reader["statsPlayerID"].ToString()!);
+            positionPlayer.PlayerId = reader["baseballPlayerGUIDIfKnown"] is not byte[] bytes ? null : bytes.ToGuid();
+            positionPlayer.CurrentTeam = reader["currentTeamName"].ToString()!;
+            positionPlayer.MostRecentTeam = reader["mostRecentTeamName"].ToString()!;
+            positionPlayer.SecondMostRecentTeam = reader["secondMostRecentTeamName"].ToString()!;
+            positionPlayer.FirstName = reader["firstName"].ToString()!;
+            positionPlayer.LastName = reader["lastName"].ToString()!;
+
+            positionPlayer.RetirementSeason = string.IsNullOrEmpty(reader["retirementSeason"].ToString())
+                ? null
+                : int.Parse(reader["retirementSeason"].ToString()!);
+
+            positionPlayer.RetirementAge = string.IsNullOrEmpty(reader["age"].ToString())
+                ? null
+                : int.Parse(reader["age"].ToString()!);
+
+            positionPlayer.PrimaryPositionNumber = int.Parse(reader["primaryPosition"].ToString()!);
+            positionPlayer.SecondaryPositionNumber = string.IsNullOrEmpty(reader["secondaryPosition"].ToString())
+                ? null
+                : int.Parse(reader["secondaryPosition"].ToString()!);
+
+            positionPlayer.PitcherRole = string.IsNullOrEmpty(reader["pitcherRole"].ToString())
+                ? null
+                : int.Parse(reader["pitcherRole"].ToString()!);
+
+            positionPlayer.GamesPlayed = int.Parse(reader["gamesPlayed"].ToString()!);
+            positionPlayer.GamesBatting = int.Parse(reader["gamesBatting"].ToString()!);
+            positionPlayer.AtBats = int.Parse(reader["atBats"].ToString()!);
+            positionPlayer.Runs = int.Parse(reader["runs"].ToString()!);
+            positionPlayer.Hits = int.Parse(reader["hits"].ToString()!);
+            positionPlayer.Doubles = int.Parse(reader["doubles"].ToString()!);
+            positionPlayer.Triples = int.Parse(reader["triples"].ToString()!);
+            positionPlayer.HomeRuns = int.Parse(reader["homeruns"].ToString()!);
+            positionPlayer.RunsBattedIn = int.Parse(reader["rbi"].ToString()!);
+            positionPlayer.StolenBases = int.Parse(reader["stolenBases"].ToString()!);
+            positionPlayer.CaughtStealing = int.Parse(reader["caughtStealing"].ToString()!);
+            positionPlayer.Walks = int.Parse(reader["baseOnBalls"].ToString()!);
+            positionPlayer.Strikeouts = int.Parse(reader["strikeOuts"].ToString()!);
+            positionPlayer.HitByPitch = int.Parse(reader["hitByPitch"].ToString()!);
+            positionPlayer.SacrificeHits = int.Parse(reader["sacrificeHits"].ToString()!);
+            positionPlayer.SacrificeFlies = int.Parse(reader["sacrificeFlies"].ToString()!);
+            positionPlayer.Errors = int.Parse(reader["errors"].ToString()!);
+            positionPlayer.PassedBalls = int.Parse(reader["passedBalls"].ToString()!);
+
             yield return positionPlayer;
         }
     }
@@ -54,19 +91,51 @@ public partial class DataService
         {
             Value = _applicationContext.SelectedFranchise!.LeagueId.ToBlob()
         });
-        
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile<CareerPitchingStatisticMapper>();
-        });
-        
-        var mapper = new Mapper(config);
 
         var reader = await command.ExecuteReaderAsync();
 
         while (reader.Read())
         {
-            var pitcher = mapper.Map<CareerPitchingStatistic>(reader);
+            var pitcher = new CareerPitchingStatistic();
+
+            pitcher.AggregatorId = int.Parse(reader["aggregatorID"].ToString()!);
+            pitcher.StatsPlayerId = int.Parse(reader["statsPlayerID"].ToString()!);
+            pitcher.PlayerId = reader["baseballPlayerGUIDIfKnown"] is not byte[] bytes ? null : bytes.ToGuid();
+            pitcher.CurrentTeam = reader["currentTeamName"].ToString()!;
+            pitcher.MostRecentTeam = reader["mostRecentTeamName"].ToString()!;
+            pitcher.SecondMostRecentTeam = reader["secondMostRecentTeamName"].ToString()!;
+            pitcher.FirstName = reader["firstName"].ToString()!;
+            pitcher.LastName = reader["lastName"].ToString()!;
+
+            pitcher.RetirementSeason = string.IsNullOrEmpty(reader["retirementSeason"].ToString())
+                ? null
+                : int.Parse(reader["retirementSeason"].ToString()!);
+
+            pitcher.RetirementAge = string.IsNullOrEmpty(reader["age"].ToString())
+                ? null
+                : int.Parse(reader["age"].ToString()!);
+
+            pitcher.PitcherRole = int.Parse(reader["pitcherRole"].ToString()!);
+            pitcher.Wins = int.Parse(reader["wins"].ToString()!);
+            pitcher.Losses = int.Parse(reader["losses"].ToString()!);
+            pitcher.GamesPlayed = int.Parse(reader["games"].ToString()!);
+            pitcher.GamesStarted = int.Parse(reader["gamesStarted"].ToString()!);
+            pitcher.TotalPitches = int.Parse(reader["totalPitches"].ToString()!);
+            pitcher.CompleteGames = int.Parse(reader["completeGames"].ToString()!);
+            pitcher.Shutouts = int.Parse(reader["shutouts"].ToString()!);
+            pitcher.Saves = int.Parse(reader["saves"].ToString()!);
+            pitcher.OutsPitched = int.Parse(reader["outsPitched"].ToString()!);
+            pitcher.HitsAllowed = int.Parse(reader["hits"].ToString()!);
+            pitcher.EarnedRuns = int.Parse(reader["earnedRuns"].ToString()!);
+            pitcher.HomeRunsAllowed = int.Parse(reader["homeRuns"].ToString()!);
+            pitcher.WalksAllowed = int.Parse(reader["baseOnBalls"].ToString()!);
+            pitcher.Strikeouts = int.Parse(reader["strikeOuts"].ToString()!);
+            pitcher.HitByPitch = int.Parse(reader["battersHitByPitch"].ToString()!);
+            pitcher.BattersFaced = int.Parse(reader["battersFaced"].ToString()!);
+            pitcher.GamesFinished = int.Parse(reader["gamesFinished"].ToString()!);
+            pitcher.RunsAllowed = int.Parse(reader["runsAllowed"].ToString()!);
+            pitcher.WildPitches = int.Parse(reader["wildPitches"].ToString()!);
+
             yield return pitcher;
         }
     }
--- SMB3Explorer/Services/DataService/DataServiceFranchiseSeasons.cs
+++ SMB3Explorer/Services/DataService/DataServiceFranchiseSeasons.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-using AutoMapper;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
 using SMB3Explorer.Models;
-using SMB3Explorer.Models.Mappings;
 using SMB3Explorer.Utils;
 
 namespace SMB3Explorer.Services.DataService;
@@ -28,19 +27,57 @@ public partial class DataService
         {
             Value = _applicationContext.SelectedFranchise!.FranchiseId.ToBlob()
         });
-        
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile<BattingSeasonStatisticMapper>();
-        });
-
-        var mapper = new Mapper(config);
 
         var reader = await command.ExecuteReaderAsync();
 
         while (reader.Read())
         {
-            var positionPlayerStatistic = mapper.Map<BattingSeasonStatistic>(reader);
+            var positionPlayerStatistic = new BattingSeasonStatistic();
+
+            positionPlayerStatistic.FirstName = reader["firstName"].ToString()!;
+            positionPlayerStatistic.LastName = reader["lastName"].ToString()!;
+
+            var position = reader["primaryPosition"].ToString();
+            positionPlayerStatistic.PositionNumber = position is null ? 0 : int.Parse(position);
+
+            positionPlayerStatistic.SecondaryPositionNumber =
+                string.IsNullOrEmpty(reader["secondaryPosition"].ToString()!)
+                    ? null
+                    : int.Parse(reader["secondaryPosition"].ToString()!);
+            positionPlayerStatistic.CurrentTeam = string.IsNullOrEmpty(reader["currentTeam"].ToString()!)
+                ? null
+                : reader["currentTeam"].ToString()!;
+            positionPlayerStatistic.GamesPlayed = int.Parse(reader["gamesPlayed"].ToString()!);
+            positionPlayerStatistic.GamesBatting = int.Parse(reader["gamesBatting"].ToString()!);
+            positionPlayerStatistic.AtBats = int.Parse(reader["atBats"].ToString()!);
+            positionPlayerStatistic.Runs = int.Parse(reader["runs"].ToString()!);
+            positionPlayerStatistic.Hits = int.Parse(reader["hits"].ToString()!);
+            positionPlayerStatistic.Doubles = int.Parse(reader["doubles"].ToString()!);
+            positionPlayerStatistic.Triples = int.Parse(reader["triples"].ToString()!);
+            positionPlayerStatistic.HomeRuns = int.Parse(reader["homeruns"].ToString()!);
+            positionPlayerStatistic.RunsBattedIn = int.Parse(reader["rbi"].ToString()!);
+            positionPlayerStatistic.StolenBases = int.Parse(reader["stolenBases"].ToString()!);
+            positionPlayerStatistic.CaughtStealing = int.Parse(reader["caughtStealing"].ToString()!);
+            positionPlayerStatistic.Walks = int.Parse(reader["baseOnBalls"].ToString()!);
+            positionPlayerStatistic.Strikeouts = int.Parse(reader["strikeOuts"].ToString()!);
+            positionPlayerStatistic.HitByPitch = int.Parse(reader["hitByPitch"].ToString()!);
+            positionPlayerStatistic.SacrificeHits = int.Parse(reader["sacrificeHits"].ToString()!);
+            positionPlayerStatistic.SacrificeFlies = int.Parse(reader["sacrificeFlies"].ToString()!);
+            positionPlayerStatistic.Errors = int.Parse(reader["errors"].ToString()!);
+            positionPlayerStatistic.PassedBalls = int.Parse(reader["passedBalls"].ToString()!);
+            positionPlayerStatistic.CompletionDate = string.IsNullOrEmpty(reader["completionDate"].ToString()!)
+                ? null
+                : DateTime.Parse(reader["completionDate"].ToString()!);
+            positionPlayerStatistic.SeasonId = int.Parse(reader["seasonId"].ToString()!);
+            positionPlayerStatistic.PlayerId = reader["baseballPlayerGUID"] is not byte[] bytes ? null : bytes.ToGuid();
+
+            if (isRegularSeason)
+            {
+                positionPlayerStatistic.PreviousTeam = string.IsNullOrEmpty(reader["previousTeam"].ToString()!)
+                    ? null
+                    : reader["previousTeam"].ToString()!;
+            }
+
             yield return positionPlayerStatistic;
         }
     }
@@ -64,19 +101,52 @@ public partial class DataService
         {
             Value = _applicationContext.SelectedFranchise!.FranchiseId.ToBlob()
         });
-        
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile<PitchingSeasonStatisticMapper>();
-        });
-        
-        var mapper = new Mapper(config);
 
         var reader = await command.ExecuteReaderAsync();
 
         while (reader.Read())
         {
-            var pitcherStatistic = mapper.Map<PitchingSeasonStatistic>(reader);
+            var pitcherStatistic = new PitchingSeasonStatistic();
+
+            pitcherStatistic.PlayerId = reader["baseballPlayerGUID"] is not byte[] bytes ? null : bytes.ToGuid();
+            pitcherStatistic.FirstName = reader["firstName"].ToString()!;
+            pitcherStatistic.LastName = reader["lastName"].ToString()!;
+            pitcherStatistic.PositionNumber = int.Parse(reader["primaryPosition"].ToString()!);
+            pitcherStatistic.CurrentTeam = string.IsNullOrEmpty(reader["currentTeam"].ToString()!)
+                ? null
+                : reader["currentTeam"].ToString()!;
+            pitcherStatistic.PitcherRole = int.Parse(reader["pitcherRole"].ToString()!);
+            pitcherStatistic.GamesPlayed = int.Parse(reader["games"].ToString()!);
+            pitcherStatistic.GamesStarted = int.Parse(reader["gamesStarted"].ToString()!);
+            pitcherStatistic.Wins = int.Parse(reader["wins"].ToString()!);
+            pitcherStatistic.Losses = int.Parse(reader["losses"].ToString()!);
+            pitcherStatistic.CompleteGames = int.Parse(reader["completeGames"].ToString()!);
+            pitcherStatistic.Shutouts = int.Parse(reader["shutouts"].ToString()!);
+            pitcherStatistic.TotalPitches = int.Parse(reader["totalPitches"].ToString()!);
+            pitcherStatistic.Saves = int.Parse(reader["saves"].ToString()!);
+            pitcherStatistic.OutsPitched = int.Parse(reader["outsPitched"].ToString()!);
+            pitcherStatistic.HitsAllowed = int.Parse(reader["hits"].ToString()!);
+            pitcherStatistic.EarnedRuns = int.Parse(reader["earnedRuns"].ToString()!);
+            pitcherStatistic.HomeRunsAllowed = int.Parse(reader["homeRuns"].ToString()!);
+            pitcherStatistic.WalksAllowed = int.Parse(reader["baseOnBalls"].ToString()!);
+            pitcherStatistic.Strikeouts = int.Parse(reader["strikeOuts"].ToString()!);
+            pitcherStatistic.HitByPitch = int.Parse(reader["battersHitByPitch"].ToString()!);
+            pitcherStatistic.BattersFaced = int.Parse(reader["battersFaced"].ToString()!);
+            pitcherStatistic.GamesFinished = int.Parse(reader["gamesFinished"].ToString()!);
+            pitcherStatistic.RunsAllowed = int.Parse(reader["runsAllowed"].ToString()!);
+            pitcherStatistic.WildPitches = int.Parse(reader["wildPitches"].ToString()!);
+            pitcherStatistic.CompletionDate = string.IsNullOrEmpty(reader["completionDate"].ToString()!)
+                ? null
+                : DateTime.Parse(reader["completionDate"].ToString()!);
+            pitcherStatistic.SeasonId = int.Parse(reader["seasonId"].ToString()!);
+
+            if (isRegularSeason)
+            {
+                pitcherStatistic.PreviousTeam = string.IsNullOrEmpty(reader["previousTeam"].ToString()!)
+                    ? null
+                    : reader["previousTeam"].ToString()!;
+            }
+
             yield return pitcherStatistic;
         }
     }
