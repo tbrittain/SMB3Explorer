51e2cc750e69a073158ef63fb69af6e5e6286037
tbrittain <treybrittain@gmail.com>
2023-03-16 00:45:09 -0500

    Add FranchiseSeasonStandings.sql

---
+++ SMB3Explorer/Resources/Sql/FranchiseSeasonStandings.sql
@@ -0,0 +1,23 @@
+﻿WITH franchiseSeasons AS
+         (SELECT ts.*
+          FROM t_franchise tf
+                   JOIN t_leagues tl ON tf.leagueGUID = tl.GUID
+                   LEFT JOIN t_franchise_seasons tfs ON tf.GUID = tfs.franchiseGUID
+                   LEFT JOIN t_seasons ts ON tfs.seasonGUID = ts.GUID
+          WHERE tl.GUID = CAST(@leagueId AS BLOB)
+          GROUP BY ts.ID)
+SELECT ROW_NUMBER() OVER (
+    ORDER BY fs.ID, vss.conferenceGUID, vss.divisionGUID, vss.gamesBack, vss.runsFor - vss.runsAgainst DESC
+    )                                AS `index`,
+       fs.ID                         AS seasonID,
+       vss.*,
+       vss.runsFor - vss.runsAgainst AS runDifferential,
+       tc.name                       AS conferenceName,
+       td.name                       AS divisionName,
+       tt.teamName                   AS teamName
+FROM v_season_standings vss
+         JOIN franchiseSeasons fs ON vss.seasonGUID = fs.GUID
+         JOIN t_conferences tc ON vss.conferenceGUID = tc.GUID
+         JOIN t_divisions td ON vss.divisionGUID = td.GUID
+         JOIN t_teams tt ON vss.teamGUID = tt.GUID
+ORDER BY fs.ID, vss.conferenceGUID, vss.divisionGUID, vss.gamesBack, runDifferential DESC
