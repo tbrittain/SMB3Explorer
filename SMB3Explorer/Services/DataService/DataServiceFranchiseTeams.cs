674177a58fba5f1feaeae2f597b54cee621960c9
tbrittain <treybrittain@gmail.com>
2023-03-18 13:53:03 -0500

    Add SQL queries for GetFranchiseSeasonStandings and GetFranchisePlayoffStandings methods

--- SMB3Explorer/Services/DataService/DataServiceFranchiseTeams.cs
+++ SMB3Explorer/Services/DataService/DataServiceFranchiseTeams.cs
@@ -1,18 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
 using SMB3Explorer.Models;
+using SMB3Explorer.Utils;
 
 namespace SMB3Explorer.Services.DataService;
 
 public partial class DataService
 {
-    public IAsyncEnumerable<FranchiseSeasonStanding> GetFranchiseSeasonStandings()
+    public async IAsyncEnumerable<FranchiseSeasonStanding> GetFranchiseSeasonStandings()
     {
-        throw new NotImplementedException();
+        var command = Connection!.CreateCommand();
+        var commandText = SqlRunner.GetSqlCommand(SqlFile.FranchiseSeasonStandings);
+        command.CommandText = commandText;
+        var reader = await command.ExecuteReaderAsync();
+        
+        command.Parameters.Add(new SqliteParameter("@leagueId", SqliteType.Blob)
+        {
+            Value = _applicationContext.SelectedFranchise!.LeagueId.ToBlob()
+        });
+
+        
+        while (reader.Read())
+        {
+            // TODO: Implement this
+            yield return new FranchiseSeasonStanding();
+        }
     }
 
-    public IAsyncEnumerable<FranchisePlayoffStanding> GetFranchisePlayoffStandings()
+    public async IAsyncEnumerable<FranchisePlayoffStanding> GetFranchisePlayoffStandings()
     {
-        throw new NotImplementedException();
+        var command = Connection!.CreateCommand();
+        var commandText = SqlRunner.GetSqlCommand(SqlFile.FranchisePlayoffStandings);
+        command.CommandText = commandText;
+        var reader = await command.ExecuteReaderAsync();
+        
+        command.Parameters.Add(new SqliteParameter("@leagueId", SqliteType.Blob)
+        {
+            Value = _applicationContext.SelectedFranchise!.LeagueId.ToBlob()
+        });
+
+
+        while (reader.Read())
+        {
+            // TODO: Implement this
+            yield return new FranchisePlayoffStanding();
+        }
     }
 }
