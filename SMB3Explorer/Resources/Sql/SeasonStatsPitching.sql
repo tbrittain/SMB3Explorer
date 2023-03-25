76f857d716bbb58bf823b041f5f5e42cbfd53b5f
tbrittain <treybrittain@gmail.com>
2023-03-25 12:18:53 -0500

    Add additional join to franchises to ensure season numbers are selected properly

--- SMB3Explorer/Resources/Sql/SeasonStatsPitching.sql
+++ SMB3Explorer/Resources/Sql/SeasonStatsPitching.sql
@@ -6,6 +6,7 @@
                         RANK() OVER (ORDER BY id) AS seasonNum
                  FROM t_seasons
                           JOIN t_leagues ON t_seasons.historicalLeagueGUID = t_leagues.GUID
+                          JOIN t_franchise tf ON t_leagues.GUID = tf.leagueGUID
                  WHERE t_leagues.GUID = CAST(@leagueId AS BLOB))
 SELECT baseballPlayerGUID,
        tsea.completionDate,
