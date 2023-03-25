0ee83f83052f963b54d04c816a03df3c9727f6cd
tbrittain <treybrittain@gmail.com>
2023-03-25 13:56:55 -0500

    Add GetFranchiseSeasons.sql

---
+++ SMB3Explorer/Resources/Sql/GetFranchiseSeasons.sql
@@ -0,0 +1,8 @@
+﻿SELECT id                        AS seasonID,
+       historicalLeagueGUID      AS leagueGUID,
+       RANK() OVER (ORDER BY id) AS seasonNum
+FROM t_seasons
+         JOIN t_leagues ON t_seasons.historicalLeagueGUID = t_leagues.GUID
+         JOIN t_franchise tf ON t_leagues.GUID = tf.leagueGUID
+WHERE t_leagues.GUID = CAST(@leagueId AS BLOB)
+ORDER BY ID DESC
