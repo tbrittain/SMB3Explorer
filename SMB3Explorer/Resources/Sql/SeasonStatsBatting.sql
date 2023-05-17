e561f8d68eebe252a1f187ea408ebec1de7f4c65
Trey Brittain <treybrittain@gmail.com>
2023-05-17 11:10:38 -0500

    Update season/playoff batting stats with same changes for pitching

--- SMB3Explorer/Resources/Sql/SeasonStatsBatting.sql
+++ SMB3Explorer/Resources/Sql/SeasonStatsBatting.sql
@@ -8,50 +8,62 @@
                           JOIN t_leagues ON t_seasons.historicalLeagueGUID = t_leagues.GUID
                           JOIN t_franchise tf ON t_leagues.GUID = tf.leagueGUID
                  WHERE t_leagues.GUID = CAST(@leagueId AS BLOB))
-SELECT baseballPlayerGUID,
-       tsea.completionDate,
-       tsea.ID                                                                       AS seasonId,
+SELECT ts.aggregatorID                   AS aggregatorID,
+       ts.statsPlayerID                  AS statsPlayerID,
+       tbpli.GUID                        AS baseballPlayerGUIDIfKnown,
+       s.seasonID,
        s.seasonNum,
+       currentTeamLocal.GUID             AS teamGUID,
+       mostRecentTeamLocal.GUID          AS mostRecentlyPlayedTeamGUID,
+       previouslyRecentTeam.GUID         AS previousRecentlyPlayedTeamGUID,
        CASE
-           WHEN tsp.[baseballPlayerLocalID] IS NULL THEN tsp.[firstName]
-           ELSE vbpi.[firstName] END                                                 AS firstName,
+           WHEN tsp.baseballPlayerLocalID IS NULL THEN tsp.firstName
+           ELSE vbpi.firstName END       AS firstName,
        CASE
-           WHEN tsp.[baseballPlayerLocalID] IS NULL THEN tsp.[lastName]
-           ELSE vbpi.[lastName] END                                                  AS lastName,
+           WHEN tsp.baseballPlayerLocalID IS NULL THEN tsp.lastName
+           ELSE vbpi.lastName END        AS lastName,
        CASE
-           WHEN tsp.[baseballPlayerLocalID] IS NULL THEN tsp.[primaryPos]
-           ELSE vbpi.[primaryPosition] END                                           AS primaryPosition,
+           WHEN tsp.baseballPlayerLocalID IS NULL THEN tsp.primaryPos
+           ELSE vbpi.primaryPosition END AS primaryPosition,
        CASE
-           WHEN tsp.[baseballPlayerLocalID] IS NULL THEN tsp.[pitcherRole]
-           ELSE vbpi.[pitcherRole] END                                               AS pitcherRole,
-       CAST(secondaryPosition.optionValue AS INTEGER)                                AS secondaryPosition,
-       tsb.*,
-       currentTeam.teamName                                                          AS currentTeam,
-       previousTeam.teamName                                                         AS previousTeam,
-       tbp.age - (MAX(seasonNum) OVER (PARTITION BY baseballPlayerGUID) - seasonNum) AS age
-FROM [v_baseball_player_info] vbpi
-         LEFT JOIN t_baseball_player_local_ids tbpli ON vbpi.baseballPlayerGUID = tbpli.GUID
-         LEFT JOIN t_stats_players tsp ON tbpli.localID = tsp.baseballPlayerLocalID
-         LEFT JOIN t_stats ts ON tsp.statsPlayerID = ts.statsPlayerID
-         LEFT JOIN t_stats_batting tsb ON ts.aggregatorID = tsb.aggregatorID
+           WHEN tsp.baseballPlayerLocalID IS NULL THEN tsp.pitcherRole
+           ELSE vbpi.pitcherRole END     AS pitcherRole,
+       gamesBatting,
+       atBats,
+       runs,
+       hits,
+       doubles,
+       triples,
+       homeruns,
+       rbi,
+       stolenBases,
+       caughtStealing,
+       baseOnBalls,
+       strikeOuts,
+       hitByPitch,
+       sacrificeHits,
+       sacrificeFlies,
+       errors,
+       passedBalls
+FROM t_stats_batting tsb
+         JOIN t_stats ts ON tsb.aggregatorID = ts.aggregatorID
+         JOIN t_stats_players tsp USING (statsPlayerID)
+         JOIN t_season_stats tss USING (aggregatorID)
+         JOIN seasons s USING (seasonID)
 
-         LEFT JOIN t_baseball_players tbp ON tbpli.GUID = tbp.GUID
+         LEFT JOIN t_team_local_ids currentTeamLocal ON ts.currentTeamLocalID = currentTeamLocal.localID
+         LEFT JOIN t_team_local_ids mostRecentTeamLocal
+                   ON ts.mostRecentlyPlayedTeamLocalID = mostRecentTeamLocal.localID
+         LEFT JOIN t_team_local_ids previouslyRecentTeam
+                   ON ts.previousRecentlyPlayedTeamLocalID = previouslyRecentTeam.localID
 
-         LEFT JOIN t_season_stats tss ON ts.aggregatorID = tss.aggregatorID
-
-         JOIN t_seasons tsea ON tss.seasonID = tsea.ID
-         JOIN seasons s ON tsea.ID = s.seasonID
-         JOIN t_league_local_ids tlli ON tsp.leagueLocalID = tlli.localID
-         JOIN t_leagues tl ON tlli.GUID = tl.GUID
-
-         LEFT JOIN t_baseball_player_options secondaryPosition
-                   ON tbpli.localID = secondaryPosition.baseballPlayerLocalID AND secondaryPosition.optionKey = 55
-
-         LEFT JOIN [t_team_local_ids] tt1 ON ts.[currentTeamLocalID] = tt1.[localID]
-         LEFT JOIN [t_team_local_ids] tt2
-                   ON ts.[previousRecentlyPlayedTeamLocalID] = tt2.[localID]
-         LEFT JOIN teams currentTeam ON tt1.GUID = currentTeam.teamGUID
-         LEFT JOIN teams previousTeam ON tt2.GUID = previousTeam.teamGUID
-
-WHERE tl.GUID = CAST(@leagueId AS BLOB)
-ORDER BY baseballPlayerGUID, seasonNum
+         LEFT JOIN t_baseball_player_local_ids tbpli
+                   ON tsp.baseballPlayerLocalID = tbpli.localID
+         LEFT JOIN v_baseball_player_info vbpi ON tbpli.GUID =
+                                                  vbpi.baseballPlayerGUID
+WHERE 1 = CASE
+              WHEN :seasonId IS NOT NULL THEN CASE
+                                                  WHEN s.seasonID = :seasonId THEN 1
+                                                  ELSE 0 END
+              ELSE 1 END
+ORDER BY s.seasonNum, ts.statsPlayerID;
