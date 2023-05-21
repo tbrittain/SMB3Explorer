a3e0f888b4188e252130ffdce4f8877cdf3048dd
Trey Brittain <treybrittain@gmail.com>
2023-05-21 11:46:14 -0500

    Update export columns for other various exports

--- SMB3Explorer/Models/Exports/FranchiseSeasonStanding.cs
+++ SMB3Explorer/Models/Exports/FranchiseSeasonStanding.cs
@@ -7,42 +7,42 @@ namespace SMB3Explorer.Models.Exports;
 
 public class FranchiseSeasonStanding
 {
-    [Name("index"), Index(0)]
+    [Name("Index"), Index(0)]
     public int Index { get; set; }
     
-    [Name("season_id"), Index(1)]
+    [Ignore]
     public int SeasonId { get; set; }
     
-    [Name("season_num"), Index(2)]
+    [Name("Season"), Index(1)]
     public int SeasonNum { get; set; }
     
-    [Name("team_name"), Index(3)]
+    [Name("Team"), Index(2)]
     public string TeamName { get; set; } = string.Empty;
     
-    [Name("division_name"), Index(4)]
+    [Name("Division"), Index(3)]
     public string DivisionName { get; set; } = string.Empty;
     
-    [Name("conference_name"), Index(5)]
+    [Name("Conference"), Index(4)]
     public string ConferenceName { get; set; } = string.Empty;
     
-    [Name("wins"), Index(6)]
+    [Name("W"), Index(5)]
     public int Wins { get; set; }
     
-    [Name("losses"), Index(7)]
+    [Name("L"), Index(6)]
     public int Losses { get; set; }
     
-    [Name("runs_for"), Index(8)]
+    [Name("Runs For"), Index(7)]
     public int RunsFor { get; set; }
     
-    [Name("runs_against"), Index(9)]
+    [Name("Runs Against"), Index(8)]
     public int RunsAgainst { get; set; }
     
-    [Name("run_differential"), Index(10)]
+    [Name("Run Differential"), Index(9)]
     public int RunDifferential { get; set; }
     
-    [Name("win_percentage"), Index(11)]
+    [Name("WPCT"), Index(10)]
     public double WinPercentage { get; set; }
     
-    [Name("games_back"), Index(12)]
+    [Name("GB"), Index(11)]
     public double GamesBack { get; set; }
 }
