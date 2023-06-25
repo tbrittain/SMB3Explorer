42698e400b85854c8eb8e0c3d8966082e9c108fc
Trey Brittain <treybrittain@gmail.com>
2023-06-25 17:18:46 -0500

    Add SMB4 compatibility in LandingView and LandingViewModel

---
+++ SMB3Explorer/Enums/SelectedGame.cs
@@ -0,0 +1,7 @@
+﻿namespace SMB3Explorer.Enums;
+
+public enum SelectedGame
+{
+    Smb3,
+    Smb4
+}
--- SMB3Explorer/ViewModels/LandingViewModel.cs
+++ SMB3Explorer/ViewModels/LandingViewModel.cs
@@ -6,6 +6,7 @@ using CommunityToolkit.Mvvm.Input;
 using OneOf;
 using OneOf.Types;
 using Serilog;
+using SMB3Explorer.Services.ApplicationContext;
 using SMB3Explorer.Services.DataService;
 using SMB3Explorer.Services.NavigationService;
 using SMB3Explorer.Services.SystemIoWrapper;
@@ -15,20 +16,52 @@ namespace SMB3Explorer.ViewModels;
 
 public partial class LandingViewModel : ViewModelBase
 {
+    // ReSharper disable once ConvertToConstant.Global
+    public static readonly string Smb3 = "SMB3";
+
+    // ReSharper disable once ConvertToConstant.Global
+    public static readonly string Smb4 = "SMB4";
     private readonly IDataService _dataService;
     private readonly INavigationService _navigationService;
     private readonly ISystemIoWrapper _systemIoWrapper;
+    private readonly IApplicationContext _applicationContext;
+    private string _selectedGame = Smb4;
 
-    public LandingViewModel(IDataService dataService, INavigationService navigationService, ISystemIoWrapper systemIoWrapper)
+    public LandingViewModel(IDataService dataService, INavigationService navigationService,
+        ISystemIoWrapper systemIoWrapper, IApplicationContext applicationContext)
     {
         _navigationService = navigationService;
         _systemIoWrapper = systemIoWrapper;
+        _applicationContext = applicationContext;
         _dataService = dataService;
-        
+
         Log.Information("Initializing LandingViewModel");
 
         _dataService.ConnectionChanged += DataServiceOnConnectionChanged;
     }
+
+    public string SelectedGame
+    {
+        get => _selectedGame;
+        set
+        {
+            SetField(ref _selectedGame, value);
+            _applicationContext.SelectedGame = value switch
+            {
+                "SMB3" => Enums.SelectedGame.Smb3,
+                "SMB4" => Enums.SelectedGame.Smb4,
+                _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+            };
+            
+            OnPropertyChanged(nameof(Smb3ButtonVisibility));
+            OnPropertyChanged(nameof(Smb4ButtonVisibility));
+        }
+    }
+
+    private bool IsSmb3Selected => SelectedGame == Smb3;
+    
+    public Visibility Smb3ButtonVisibility => IsSmb3Selected ? Visibility.Visible : Visibility.Collapsed;
+    public Visibility Smb4ButtonVisibility => IsSmb3Selected ? Visibility.Collapsed : Visibility.Visible;
 
     private void DataServiceOnConnectionChanged(object? sender, EventArgs e)
     {
@@ -47,14 +80,14 @@ public partial class LandingViewModel : ViewModelBase
     {
         Mouse.OverrideCursor = Cursors.Wait;
 
-        var filePathResult =  SaveFile.GetSaveFilePath(_systemIoWrapper);
+        var filePathResult = SaveFile.GetSaveFilePath(_systemIoWrapper);
         if (filePathResult.TryPickT1(out _, out var filePath) || string.IsNullOrEmpty(filePath))
         {
             // TODO: Handle error
             Mouse.OverrideCursor = Cursors.Arrow;
             return;
         }
-        
+
         var connectionResult = await EstablishDbConnection(filePath);
         HandleDatabaseConnection(connectionResult);
     }
@@ -109,7 +142,7 @@ public partial class LandingViewModel : ViewModelBase
     {
         var hasError = false;
         var connectionResult = await _dataService.EstablishDbConnection(filePath, isCompressedSaveGame);
-        
+
         if (connectionResult.TryPickT1(out var error, out _))
         {
             hasError = true;
@@ -118,7 +151,7 @@ public partial class LandingViewModel : ViewModelBase
         }
 
         Application.Current.Dispatcher.Invoke(() => Mouse.OverrideCursor = Cursors.Arrow);
-        
+
         return !hasError ? new Success() : new Error();
     }
 
