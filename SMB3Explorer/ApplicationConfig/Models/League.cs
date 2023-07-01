43bc2398a0cd43e33412bd0dabf1fd32e226ae90
Trey Brittain <treybrittain@gmail.com>
2023-07-01 09:56:48 -0500

    Add new properties to League config

--- SMB3Explorer/ApplicationConfig/Models/League.cs
+++ SMB3Explorer/ApplicationConfig/Models/League.cs
@@ -7,7 +7,22 @@ public class League
 {
     [JsonProperty("name")]
     public string Name { get; set; } = string.Empty;
-    
+
     [JsonProperty("id")]
     public Guid Id { get; set; }
+
+    [JsonProperty("playerTeam")]
+    public string? PlayerTeam { get; set; }
+    
+    [JsonProperty("numSeasons")]
+    public int NumSeasons { get; set; }
+    
+    [JsonProperty("numTimesAccessed")]
+    public int NumTimesAccessed { get; set; }
+
+    [JsonProperty("firstAccessed")]
+    public DateTime FirstAccessed { get; set; }
+
+    [JsonProperty("lastAccessed")]
+    public DateTime LastAccessed { get; set; }
 }
