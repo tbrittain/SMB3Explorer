dee766950cfc5f5172b5fc073425eb6a9cb99347
Trey Brittain <treybrittain@gmail.com>
2025-04-21 21:12:31 -0500

    Adjust FranchisePlayoffStandings.sql to support season/elim

--- SMB3Explorer/Resources/Sql/FranchisePlayoffStandings.sql
+++ SMB3Explorer/Resources/Sql/FranchisePlayoffStandings.sql
@@ -1,9 +1,7 @@
-﻿WITH franchiseSeasons AS
+﻿WITH leagueSeasons AS
          (SELECT ts.*
-          FROM t_franchise tf
-                   JOIN t_leagues tl ON tf.leagueGUID = tl.GUID
-                   LEFT JOIN t_franchise_seasons tfs ON tf.GUID = tfs.franchiseGUID
-                   LEFT JOIN t_seasons ts ON tfs.seasonGUID = ts.GUID
+          FROM t_seasons ts
+                   JOIN t_leagues tl ON ts.historicalLeagueGUID = tl.GUID
           WHERE tl.GUID = CAST(@leagueId AS BLOB)
           GROUP BY ts.ID),
      seasonPlayoffs AS (SELECT ts.ID AS seasonID,
@@ -28,18 +26,18 @@
                                  JOIN t_playoffs tp ON tp.GUID = vpgwl1.playoffGUID
                                  JOIN t_seasons ts ON ts.GUID = tp.seasonGUID)
 SELECT ROW_NUMBER() OVER (
-    ORDER BY fs.ID, gamesWon DESC
-    )                                AS `index`,
-       fs.ID                         AS seasonID,
-       DENSE_RANK() OVER (ORDER BY fs.ID  ) AS seasonNum,
+    ORDER BY ls.ID, gamesWon DESC
+    )                                      AS `index`,
+       ls.ID                               AS seasonID,
+       DENSE_RANK() OVER (ORDER BY ls.ID ) AS seasonNum,
        vss.*,
-       vss.runsFor - vss.runsAgainst AS runDifferential,
-       tc.name                       AS conferenceName,
-       td.name                       AS divisionName,
-       tt.teamName                   AS teamName
+       vss.runsFor - vss.runsAgainst       AS runDifferential,
+       tc.name                             AS conferenceName,
+       td.name                             AS divisionName,
+       tt.teamName                         AS teamName
 FROM seasonPlayoffs vss
-         JOIN franchiseSeasons fs ON vss.seasonID = fs.ID
+         JOIN leagueSeasons ls ON vss.seasonID = ls.ID
          JOIN t_conferences tc ON vss.conferenceGUID = tc.GUID
          JOIN t_divisions td ON vss.divisionGUID = td.GUID
          JOIN t_teams tt ON vss.teamGUID = tt.GUID
-ORDER BY fs.ID, gamesWon DESC
+ORDER BY ls.ID, gamesWon DESC
